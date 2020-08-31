e554c014500dad7b2cc91d61b44f2f19b5fbf310
Weian.T <jf6316@gmail.com>
2020-08-31 17:00:14 +0800

    Init version

---
+++ Makefile
@@ -0,0 +1,16 @@
+KERNELDIR= /home/user/ONL/20200824/OpenNetworkLinux/packages/base/amd64/kernels/kernel-4.9-lts-x86-64-all/builds/jessie/linux-4.9.75
+
+PWD:=$(shell pwd)
+INSTALLDIR=$(PWD)
+CC=gcc
+
+obj-m += x86-64-cameo-esc602-32q.o
+x86-64-cameo-esc602-32q-objs := x86-64-cameo-esc602-32q-common.o x86-64-cameo-esc602-32q-sys.o \
+x86-64-cameo-esc602-32q-led.o x86-64-cameo-esc602-32q-fan.o x86-64-cameo-esc602-32q-power.o \
+x86-64-cameo-esc602-32q-thermal.o x86-64-cameo-esc602-32q-qsfp.o
+
+modules:
+	$(MAKE) -C $(KERNELDIR) M=$(PWD) modules
+clean:     
+	rm -rf *.o *.ko *.mod.c *.markers *.order *.symvers
+.PHONY:modules clean
---
+++ x86-64-cameo-esc602-32q-thermal.h
@@ -0,0 +1,29 @@
+/* register offset define */
+#define TEMP_R_B_F_REG      0x10
+#define TEMP_R_B_B_REG      0x11
+#define TEMP_R_B_MAX_REG    0x12
+#define TEMP_R_B_MIN_REG    0x13
+#define TEMP_R_B_CRIT_REG   0x14
+#define TEMP_R_B_LCRIT_REG  0x15
+    
+#define TEMP_L_B_F_REG      0x20
+#define TEMP_L_B_B_REG      0x21
+#define TEMP_L_B_MAX_REG    0x22
+#define TEMP_L_B_MIN_REG    0x23
+#define TEMP_L_B_CRIT_REG   0x24
+#define TEMP_L_B_LCRIT_REG  0x25
+    
+#define TEMP_R_T_F_REG      0x30
+#define TEMP_R_T_B_REG      0x31
+#define TEMP_R_T_MAX_REG    0x32
+#define TEMP_R_T_MIN_REG    0x33
+#define TEMP_R_T_CRIT_REG   0x34
+#define TEMP_R_T_LCRIT_REG  0x35
+    
+#define TEMP_L_T_F_REG      0x40
+#define TEMP_L_T_B_REG      0x41
+#define TEMP_L_T_MAX_REG    0x42
+#define TEMP_L_T_MIN_REG    0x43
+#define TEMP_L_T_CRIT_REG   0x44
+#define TEMP_L_T_LCRIT_REG  0x45
+/* end of register offset define */
